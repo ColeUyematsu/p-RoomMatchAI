34bbb129132fdb7961006e62612a4b248a62d779
Mark Wang <48261076+markw56127@users.noreply.github.com>
2024-10-27 15:57:00 -0700

    added unprojected method to KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -45,5 +45,27 @@ for cluster_id, users in clusters_dict.items():
     print(f"Group {cluster_id + 1}: {users}")
 
 
+'''Unprojected -- Maybe better? (NOTE: they have different results)'''
+# scaledX = StandardScaler().fit_transform(X)
+# kmeans = KMeans(n_clusters=5, random_state=0, n_init="auto").fit(scaledX)
+# clusters = kmeans.cluster_centers_
+
+# plt.scatter(scaledX[:,0],scaledX[:,1],c="blue")
+# plt.scatter(clusters[:,0],clusters[:,1],c="orange")
+
+# y_preds = kmeans.predict(scaledX)
+
+# headers = data.columns.tolist()[1:] # each user in order
+
+# clusters_dict = {i: [] for i in range(5)}  # Creating a dictionary for clusters
+
+# for i in range(len(y_preds)):
+#     clusters_dict[y_preds[i]].append(headers[i])
+
+# # Print grouped users
+# for cluster_id, users in clusters_dict.items():
+#     print(f"Group {cluster_id + 1}: {users}")
+
+
 plt.show()
 
